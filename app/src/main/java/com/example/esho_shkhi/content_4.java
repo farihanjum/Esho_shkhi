7aa9062c3330ae75877e3fdeb0168434bcfde66e
farihanjum <farihaanjum.24csedu.022@gmail.com>
2019-10-15 21:27:30 +0600

    new'

---
+++ app/src/main/java/com/example/esho_shkhi/content_4.java
@@ -0,0 +1,13 @@
+package com.example.esho_shkhi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class content_4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content_4);
+    }
+}
